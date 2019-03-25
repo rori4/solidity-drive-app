fa53518eaca1605cdcedcbc8e2aaab843f422f86
rori4 <RangelStoilov@students.softuni.bg>
2019-03-25 17:24:45 +0200

    solidity drive app skeleton

---
+++ client/src/App.js
@@ -0,0 +1,57 @@
+import React, { Component } from "react";
+import SolidityDriveContract from "./contracts/SolidityDrive.json";
+import getWeb3 from "./utils/getWeb3";
+
+import "./App.css";
+
+class App extends Component {
+  state = { solidityDrive: [], web3: null, accounts: null, contract: null };
+
+  componentDidMount = async () => {
+    try {
+      // Get network provider and web3 instance.
+      const web3 = await getWeb3();
+
+      // Use web3 to get the user's accounts.
+      const accounts = await web3.eth.getAccounts();
+
+      // Get the contract instance.
+      const networkId = await web3.eth.net.getId();
+      const deployedNetwork = SolidityDriveContract.networks[networkId];
+      const instance = new web3.eth.Contract(
+        SolidityDriveContract.abi,
+        deployedNetwork && deployedNetwork.address,
+      );
+
+      // Set web3, accounts, and contract to the state, and then proceed with an
+      // example of interacting with the contract's methods.
+      this.setState({ web3, accounts, contract: instance }, this.runExample);
+    } catch (error) {
+      // Catch any errors for any of the above operations.
+      alert(
+        `Failed to load web3, accounts, or contract. Check console for details.`,
+      );
+      console.error(error);
+    }
+  };
+
+  getFiles = async () => {
+    //TODO:
+  }
+
+  onDrop= async () => {
+    //TODO:
+  }
+
+  render() {
+    if (!this.state.web3) {
+      return <div>Loading Web3, accounts, and contract...</div>;
+    }
+    return (
+     null
+     //TODO:
+    );
+  }
+}
+
+export default App;
